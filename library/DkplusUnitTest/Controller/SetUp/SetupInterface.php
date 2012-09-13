b431f3ca636f2694c7954659055955898faafd18
Oskar Bley <oskar@programming-php.net>
2012-09-14 00:15:24 +0200

    SetupInterface -> SetUpInterface

---
+++ library/DkplusUnitTest/Controller/SetUp/SetupInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @category   DkplusUnitTest
+ * @package    Controller
+ * @subpackage SetUp
+ * @author     Oskar Bley <oskar@programming-php.net>
+ * @link       http://github.com/UFOMelkor/DkplusUnitTest
+ */
+
+namespace DkplusUnitTest\Controller\SetUp;
+
+/**
+ * @category   DkplusUnitTest
+ * @package    Controller
+ * @subpackage SetUp
+ * @author     Oskar Bley <oskar@programming-php.net>
+ * @link       http://github.com/UFOMelkor/DkplusUnitTest
+ */
+interface SetUpInterface
+{
+    public function setUpController($controller);
+}
+
